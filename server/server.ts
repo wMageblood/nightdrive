3e10ef9bfc558dd7ac78e5388ab2d4d49422dafe
wMageblood <enzo.luciano@outlook.es>
2026-04-22 01:33:25 -0600

    fixed some stuff

--- server/server.ts
+++ server/server.ts
@@ -11,6 +11,7 @@ app.use(express.json())
 
 dotenv.config({ path: "./config.env" });
 
+
 mongoose.connect(process.env.ATLAS_URI!)
   .then(() => console.log(" ✅ MongoDB connected"))
   .catch((err) => console.error("❌ Mongo error:", err))
@@ -24,8 +25,31 @@ app.post("/apply", async (req, res) => {
   try {
 
     const data = req.body
-
     const saved = await Application.create(data);
+
+    await fetch(process.env.DISCORD_WEBHOOK_URL!, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        embeds: [
+          {
+            title: "📩 New Application",
+            color: 0xc174b6,
+            fields: [
+              { name: "Main", value: data.main, inline: true },
+              { name: "Alt", value: data.alt, inline: true },
+              { name: "Spec", value: data.mainClassOption, inline: true },
+              { name: "Experience", value: data.experience },
+              { name: "Availability", value: data.availability },
+              { name: "Reasons", value: data.reasons },
+              { name: "Contact", value: data.contact },
+            ],
+          },
+        ],
+      }),
+    });
 
     res.json({
       success: true,
