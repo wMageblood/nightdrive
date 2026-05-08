8df53a2735b8c71037594b56a7ef338cfd1a511b
wMageblood <enzo.luciano@outlook.es>
2026-05-08 01:39:58 -0600

    abstracting more

--- server/server.ts
+++ server/server.ts
@@ -3,9 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import { Application } from "./models/applications"
-import normalizeString from "./utils/normalizeString";
-import { CLASS_EMOJIS, CLASS_COLORS, ROLE_EMOJIS, APPLY_PHRASES } from "./constants/embededMessageProperties"
 import { validateCharacter } from "./services/raiderIOService";
+import { sendWebhook } from "./services/discordWebhookService";
 
 const app = express();
 
@@ -33,54 +32,9 @@ app.post("/apply", async (req, res) => {
     const data = req.body;
     const saved = await Application.create(data);
 
-    const test = async () => {
-      validateCharacter({
-        name: data.name,
-        realm: data.realm
-      })
-    }
+    await validateCharacter({ name: data.name, realm: data.realm })
 
-    test();
-
-
-    const mainIcon = CLASS_EMOJIS[data.main] || "mainIcon not found";
-    const altIcon = CLASS_EMOJIS[data.alt] || "altIcon not found";
-    const roleIcon = ROLE_EMOJIS[data.role] || "roleIcon not found";
-    const classColors = CLASS_COLORS[data.main] || 0xffffff;
-    const randomPhrase = APPLY_PHRASES[Math.floor(Math.random() * APPLY_PHRASES.length)]
-
-    await fetch(process.env.DISCORD_ASCENDED_WEBHOOK_URL!, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        content: `# ${randomPhrase}`,
-        embeds: [
-          {
-            title: `${data.name} - ${data.realm}`,
-            description: [
-              `<:raiderioicon:1500995575867248680> [RaiderIO](https://www.raider.io/characters/us/${normalizeString(data.realm)}/${normalizeString(data.name)})`,
-              `<:wlogsicon:1500994865356476577> [Logs](https://www.warcraftlogs.com/character/us/${normalizeString(data.realm)}/${normalizeString(data.name)})`,
-              `<:wowicon:1501033954080194660> [Armory](https://worldofwarcraft.com/en-us/character/us/${normalizeString(data.realm)}/${normalizeString(data.name)})`,
-            ].join("\n"),
-            color: classColors,
-            fields: [
-              { name: "Main", value: `${mainIcon} ${data.main}`, inline: true },
-              { name: "Alt", value: `${altIcon} ${data.alt}`, inline: true },
-              { name: "Role", value: `${roleIcon} ${data.role}`, inline: true },
-              { name: "__Experiencia__:", value: data.experience },
-              { name: "__Disponibilidad horaria__:", value: data.availability },
-              { name: "__Razones para aplicar__:", value: data.reasons },
-              { name: "__Información de contacto__:", value: data.contact },
-            ],
-            footer: {
-              text: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-            },
-          },
-        ],
-      }),
-    });
+    await sendWebhook(data)
 
     res.json({
       success: true,
--- server/services/discordWebhookService.ts
+++ server/services/discordWebhookService.ts
@@ -1,4 +1,5 @@
-import { CLASS_EMOJIS, CLASS_COLORS, ROLE_EMOJIS, APPLY_PHRASES } from "./constants/embededMessageProperties"
+import { CLASS_EMOJIS, CLASS_COLORS, ROLE_EMOJIS, APPLY_PHRASES } from "../constants/embededMessageProperties"
+import normalizeString from "../utils/normalizeString"
 
 interface discordProps {
   experience: string,
@@ -32,9 +33,9 @@ export const sendWebhook = async ({ experience, availability, reasons, contact, 
         {
           title: `${name} - ${realm}`,
           description: [
-            `<:raiderioicon:1500995575867248680> [RaiderIO](https://www.raider.io/characters/us/${normalizeString(data.realm)}/${normalizeString(data.name)})`,
-            `<:wlogsicon:1500994865356476577> [Logs](https://www.warcraftlogs.com/character/us/${normalizeString(data.realm)}/${normalizeString(data.name)})`,
-            `<:wowicon:1501033954080194660> [Armory](https://worldofwarcraft.com/en-us/character/us/${normalizeString(data.realm)}/${normalizeString(data.name)})`,
+            `<:raiderioicon:1500995575867248680> [RaiderIO](https://www.raider.io/characters/us/${normalizeString(realm)}/${normalizeString(name)})`,
+            `<:wlogsicon:1500994865356476577> [Logs](https://www.warcraftlogs.com/character/us/${normalizeString(realm)}/${normalizeString(name)})`,
+            `<:wowicon:1501033954080194660> [Armory](https://worldofwarcraft.com/en-us/character/us/${normalizeString(realm)}/${normalizeString(name)})`,
           ].join("\n"),
           color: classColors,
           fields: [
