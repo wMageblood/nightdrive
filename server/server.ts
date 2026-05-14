4bfe581b4f08a97611fedf8a01c747a68e711318
wMageblood <enzo.luciano@outlook.es>
2026-05-14 17:24:13 -0600

    fix memory issues

--- server/server.ts
+++ server/server.ts
@@ -18,10 +18,6 @@ const PORT = Number(process.env.PORT) || 3001;
 app.use(cors())
 app.use(express.json())
 
-
-console.log("Starting server...");
-console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
-
 mongoose.connect(process.env.ATLAS_URI!)
   .then(() => console.log(" ✅ MongoDB connected"))
   .catch((err) => console.error("❌ Mongo error:", err))
@@ -31,31 +27,31 @@ app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on ${PORT}`);
 });
 
-app.post("/apply", async (req, res) => {
+// app.post("/apply", async (req, res) => {
 
 
-  try {
+//   try {
 
-    const data = req.body;
+//     const data = req.body;
 
-    await validateCharacter({ name: data.name, realm: data.realm })
+//     await validateCharacter({ name: data.name, realm: data.realm })
 
-    const saved = await Application.create(data);
+//     const saved = await Application.create(data);
 
-    await sendWebhook(data)
+//     await sendWebhook(data)
 
-    res.json({
-      success: true,
-      data: saved,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      success: false,
-      message: "failed to save application"
-    });
-  }
-})
+//     res.json({
+//       success: true,
+//       data: saved,
+//     });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({
+//       success: false,
+//       message: "failed to save application"
+//     });
+//   }
+// })
 
 // app.get("/", async (req, res) => {
 //   res.send("server is running /roster")
