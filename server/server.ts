424a7357918e307989f6ec2a9ce2c000bc058e06
wMageblood <enzo.luciano@outlook.es>
2026-05-18 18:21:54 -0600

    ruling everything out

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,8 @@
-console.log("SERVER FILE STARTED");
-console.log("PORT:", process.env.PORT);
-console.log("ATLAS_URI EXISTS:", !!process.env.ATLAS_URI);
-console.log("WEBHOOK EXISTS:", !!process.env.DISCORD_ASCENDED_WEBHOOK_URL);
-
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import { Application } from "./models/applications"
-import { validateCharacter } from "./services/raiderIOService";
-import { sendWebhook } from "./services/discordWebhookService";
 dotenv.config();
 
 const app = express();
@@ -20,76 +13,30 @@ app.use(express.json())
 
 console.log("ATLAS_URI:", process.env.ATLAS_URI);
 
-async function startServer() {
-  try {
-    mongoose.set("bufferCommands", false);
-    await mongoose.connect(process.env.ATLAS_URI!);
-    console.log("MONGO STATE:", mongoose.connection.readyState);
-    console.log("✅ MongoDB connected");
+await mongoose.connect(process.env.ATLAS_URI!)
+  .then(() => console.log(" ✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB error:", err))
 
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Server running on ${PORT}`);
-    });
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`);
+});
 
-  } catch (err) {
-    console.error("❌ Mongo connection failed:", err);
-  }
-}
-
-startServer();
-
-app.post("/apply", async (req, res) => {
+// app.post("/apply", async (req, res) => {
 
 
-  try {
-    console.log("HIT /apply");
+//   try {
 
-    const safeData = {
-      name: req.body.name,
-      realm: req.body.realm,
-    };
 
-    console.log("SAFE DATA:", safeData);
+//     const data = req.body;
 
-    const doc = new Application(safeData);
-    console.log("MODEL CREATED");
-    const saved = await doc.save();
-    console.log("SAVE COMPLETED");
+//     const saved = await Application.create(data);
 
-    return res.json({
-      success: true,
-      data: "saved",
-    });
-
-  } catch (err) {
-    console.error("MONGO ERROR:", err);
-    return res.status(500).json({ ok: false });
-  }
-
-  // try {
-
-  //   console.log("HIT /apply");
-
-  //   const data = req.body;
-
-  //   console.log("DATA TYPE:", typeof data);
-  //   console.log("DATA KEYS:", Object.keys(data));
-  //   console.log("RAW DATA:", data);
-  //   await validateCharacter({ name: data.name, realm: data.realm })
-
-  //   const saved = await Application.create(data);
-
-  //   await sendWebhook(data)
-
-  //   res.json({
-  //     success: true,
-  //     data: saved,
-  //   });
-  // } catch (err) {
-  //   console.error(err);
-  //   return res.status(500).json({ ok: false })
-  // }
-})
+//     res.json({ success: true, data: saved, });
+//   } catch (err) {
+//     console.error(err);
+//     return res.status(500).json({ ok: false })
+//   }
+// })
 
 app.get("/", async (req, res) => {
   res.send("server is alive")
@@ -99,6 +46,6 @@ app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 })
 
-// app.get("/roster", (req, res) => {
-//   res.send("server is running /roster")
-// })
+app.get("/roster", (req, res) => {
+  res.send("server is running /roster")
+})
