1017ea446a65ba370abaf2c1b9b8c95b6cb3955b
wMageblood <enzo.luciano@outlook.es>
2026-05-20 17:33:22 -0600

    extra validation alyer

--- server/services/raiderIOService.ts
+++ server/services/raiderIOService.ts
@@ -6,7 +6,7 @@ interface characterProps {
 
 export const validateCharacter = async ({ name, realm }: characterProps) => {
 
-  const response = await fetch(`https://raider.io/api/v1/characters/profile?region=us&realm=${realm}&name=${name}&fields=gear`)
+  const response = await fetch(`https://raider.io/api/v1/characters/profile?region=us&realm=${realm}&name=${name}&fields=gear%2Cmythic_plus_scores_by_season%3Acurrent`)
 
   try {
 
@@ -17,8 +17,12 @@ export const validateCharacter = async ({ name, realm }: characterProps) => {
     const data = await response.json()
 
     if (data.gear?.item_level_equipped <= 279) {
-      throw new Error('ilvl aint enough bitch')
+      if (data.mythic_plus_scores_by_season.scores.all >= 3000) {
+        throw new Error('bro, validation error.')
+      }
     }
+
+
 
   } catch (err) {
     throw new Error('Character wasnt valid.')
