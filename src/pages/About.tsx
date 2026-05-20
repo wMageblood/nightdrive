81c41b556c6139d4f50a8be8fa221a65df870b14
wMageblood <enzo.luciano@outlook.es>
2026-05-19 20:59:46 -0600

    fixing sobre nosotros margin

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex items-center justify-center mb-10 mt-25">
+      <div className="flex items-center justify-center mb-10 mt-25 mx-10">
         <AboutTitle />
       </div>
 
@@ -17,7 +17,7 @@ const About = () => {
         <p className="my-5">
           Juntos han logrado domesticar latinos salvajes, peinarlos, bañarlos y enseñarles a prender una computadora para poder completar su roster de raideo.
         </p>
-        <p className="my-5 mb-10">
+        <p className="my-5 xl:mb-5 mb-10">
           Contamos con gente de cada rincón de Latinoamérica, desde la seca y verdulera Bolivia hasta la irrelevante y triste Uruguay; no nos olvidemos de México, donde sus raiders viven perdiendo la cabeza (literalmente). <br/>
           En Ascended, nuestra meta siempre es la misma: tratar de llegar al último boss del parche antes de que se nos acaben las neuronas para poder esquivar el fuego, hacer la rotación de 3 botones y no perder 300 puestos en el ranking mundial (aún no lo logramos). <br/>
           Al final del día, nuestro sueño es poder ser tan felices en el juego como lo somos en la vida real (no lo somos).
