08a250880534200a81b7d2dcc05480bfddff37cd
wMageblood <enzo.luciano@outlook.es>
2026-05-20 15:19:15 -0600

    adding extra paddin on bosses

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,7 +21,7 @@ const Home = () => {
           <p className={`text-xl font-bold font-Roboto text-nowrap`}>Realm: <span className={`${rankingColor(1)}`}>1</span></p>
         </div>
 
-      <div className="grid xl:grid-cols-9 grid-cols-3 justify-center mx-auto xl:gap-3 gap-0 w-fit px-3 py-2">
+      <div className="grid xl:grid-cols-9 grid-cols-3 justify-center mx-auto xl:gap-3 gap-0 w-fit px-3 py-4">
         {MIDNIGHT_RAIDS.map(({bossImage, defeated, boss}) => (<img key={boss} className={`${defeated ? "saturate-5" : null} min-w-32 w-32 min-h-37 h-37`} src={bossImage} />))}
       </div>
     </div>
