297a21017b4293952f23d875718cfb867854ca2b
wMageblood <enzo.luciano@outlook.es>
2026-05-20 18:26:06 -0600

    testing new logic

--- server/services/raiderIOService.ts
+++ server/services/raiderIOService.ts
@@ -17,10 +17,9 @@ export const validateCharacter = async ({ name, realm }: characterProps) => {
     const data = await response.json()
 
     if (data.gear?.item_level_equipped >= 279) {
-      if (data.mythic_plus_scores_by_season.scores.all >= 3000) {
+      if (data.mythic_plus_scores_by_season?.[0]?.scores.all >= 3000) {
         throw new Error('bro, validation error.')
       }
-      throw new Error('ilvl wasnt enough bro')
     }
 
   } catch (err) {
