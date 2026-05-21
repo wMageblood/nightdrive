e9ec867c497a0aa344eb371a3040904170ce6f51
wMageblood <enzo.luciano@outlook.es>
2026-05-21 16:03:19 -0600

    deploying error/loading/success messages, button behavior

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -6,6 +6,9 @@ const initialState = { name: "", realm: "", main: "", alt: "", role: "", experie
 
 export const InputForm = () => {
 
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
   const [form, setForm] = useState({
     name: "",
     realm: "",
@@ -20,6 +23,10 @@ export const InputForm = () => {
 
   const handleSubmit = async () => {
 
+    setSuccess(false);
+    setLoading(true);
+    setError(false);
+
   const data = form;
 
     try {
@@ -31,11 +38,30 @@ export const InputForm = () => {
         body: JSON.stringify(data),
       });
 
+
       const result = await res.json()
+
+      if (!result.success) {
+        setError(true);
+    setTimeout(() => {
+        setError(false);
+      }, 6000);
+      return;
+    }
+
+      if (result.success) {
+        setSuccess(true);
+      setTimeout(() => {
+        setSuccess(false);
+      }, 6000);
+      }
+
       console.log(result);
       setForm({...initialState})
     } catch (err) {
       console.error(err);
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -70,49 +96,12 @@ export const InputForm = () => {
               <input id="reasons" className="bg-linear-to-r from-uncommon via-uncommon/10 to-uncommonfrom-uncommon/1 focus:bg-uncommon rounded-sm px-2 h-8 font-Roboto" value={form.reasons} onChange={(e) => setForm({ ...form, reasons: e.target.value})} placeholder="Razones para aplicar" type="text" />
               <input id="contact" className="bg-linear-to-r from-artifact via-artifact/10 to-artifact/1 focus:bg-artifact rounded-sm px-2 h-8 font-Roboto" value={form.contact} onChange={(e) => setForm({ ...form, contact: e.target.value})} placeholder="Discord" type="text" />
 
-            <button type="submit" className="bg-linear-to-r from-heirloom to-heirloom/1 rounded-sm px-2 h-8 uppercase font-bold font-Roboto active:bg-heirloom active:inset-shadow-lg">ENVIAR</button>
+            <button disabled={loading} onClick={handleSubmit} type="submit" className={`${loading ? "saturate-5" : null} bg-linear-to-r from-heirloom to-heirloom/1 rounded-sm px-2 h-8 uppercase font-bold font-Roboto active:bg-heirloom active:inset-shadow-lg`}>{loading ? "Enviando..." : "ENVIAR"}</button>
 
       </div>
-{/*
-      <div className="mx-auto bg-inputForm-background/15 border-2 border-gray-neutral-600 min-w-xl w-xl lg:w-3xl min-h-6xl h-6xl rounded-sm p-3 *:font-Roboto">
 
-        <div className="flex justify-between mb-8">
-          <input id="name" className="text-legendary font-bold w-2/3 h-9 mb-5 pl-2 border-b-3 border-legendary" value={form.name} onChange={(e) => setForm({ ...form, name: e.target.value})} placeholder="Nombre de tu personaje" type="text" />
-          <select id="realm" className="text-white font-bold mb-5 pl-2 py-[6.5px] appearance-none border-b-3 border-legendary" value={form.realm} onChange={(e) => setForm({ ...form, realm: e.target.value})}>
-            <option value="" className="">Reino</option>
-            {REALM_LIST.map((realm) => <option className={``} value={realm}>{realm}</option>)}
-          </select>
-        </div>
-
-          <div className="flex justify-between *:mb-16 *:pl-2">
-            <select id="main" className="bg-transparent font-bold border-b-3 border-epic appearance-none text-white mb-5 pl-2 py-1 mr-5 w-1/3" value={form.main} onChange={(e) => setForm({ ...form, main: e.target.value })}>
-                <option value="">Clase Main</option>
-                {CLASS_NAMES.map(({wowClass, value}) => <option key={value} value={value}>{wowClass}</option>)}
-            </select>
-
-            <select id="alt" className="bg-transparent font-bold border-b-3 border-epic appearance-none text-white mb-5 pl-2 py-1 w-1/3" value={form.alt} onChange={(e) => setForm({ ...form, alt: e.target.value })}>
-                <option value="">Clase Alt</option>
-                {CLASS_NAMES.map(({wowClass, value}) => <option key={value} value={value}>{wowClass}</option>)}
-            </select>
-
-            <select id="role" className="bg-transparent font-bold border-b-3 border-epic appearance-none text-white mb-5 pl-2 py-1 ml-5 w-1/3" value={form.role} onChange={(e) => setForm({...form, role: e.target.value})}>
-                <option value="">Rol</option>
-                {ROLE_NAMES.map(({role}) => <option key={role} value={role}>{role}</option>)}
-            </select>
-          </div>
-
-          <div className="*:w-full *:mb-15 *:pl-2 *:bg-gray-neutral-600/0 *:h-9 *:border-b-3">
-            <input id="experience" className="border-rare" value={form.experience} onChange={(e) => setForm({ ...form, experience: e.target.value})} placeholder="Experiencia previa" type="text" />
-            <input id="availability" className="border-uncommon" value={form.availability} onChange={(e) => setForm({ ...form, availability: e.target.value})} placeholder="Disponibilidad" type="text" />
-            <input id="reasons" className="border-poor" value={form.reasons} onChange={(e) => setForm({ ...form, reasons: e.target.value})} placeholder="Razones para aplicar" type="text" />
-            <input id="contact" className="border-artifact" value={form.contact} onChange={(e) => setForm({ ...form, contact: e.target.value})} placeholder="Discord" type="text" />
-          </div>
-
-          <div>
-            <button onClick={handleSubmit} type="submit" className="bg-heirloom font-bold tracking-tighter px-3 py-2 rounded-sm w-full">ENVIAR</button>
-          </div>
-
-      </div> */}
+      { error && <div className="flex items-center justify-center border-3 border-red-accent-700 bg-red-accent-400 rounded-sm w-1/2 h-24 px-2 py-1 font-Roboto font-bold mx-auto">Hubo un problema, asegurate de llenar todos los campos o de poner un personaje valido.</div>      }
+      { success && <div className="flex items-center justify-center border-3 border-cyan-accent-700 bg-cyan-accent-400 rounded-sm w-1/2 h-24 px-2 py-1 font-Roboto font-bold mx-auto">Tu apply fue enviado.</div> }
 
     </div>
   );
