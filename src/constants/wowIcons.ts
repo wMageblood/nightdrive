a08fdd26ff5b8193b569831087d096dade146450
wMageblood <enzo.luciano@outlook.es>
2026-05-25 00:06:49 -0600

    Testing icons

--- src/constants/wowIcons.ts
+++ src/constants/wowIcons.ts
@@ -1,17 +1,19 @@
+import * as CLASSES from "../assets/smallClassIcons/smallClassIcons"
+
 export const CLASS_NAMES = [
-  { wowClass: "DK", value: "DK", icon: "src/assets/smallClassIcons/classicon_deathknight.jpg" },
-  { wowClass: "DH", value: "DH", icon: "src/assets/smallClassIcons/classicon_demonhunter (1).jpg" },
-  { wowClass: "Druid", value: "Druid", icon: "src/assets/smallClassIcons/classicon_druid (1).jpg" },
-  { wowClass: "Evoker", value: "Evoker", icon: "src/assets/smallClassIcons/classicon_evoker (1).jpg" },
-  { wowClass: "Hunter", value: "Hunter", icon: "src/assets/smallClassIcons/classicon_hunter (1).jpg" },
-  { wowClass: "Mage", value: "Mage", icon: "src/assets/smallClassIcons/classicon_mage (1).jpg" },
-  { wowClass: "Monk", value: "Monk", icon: "src/assets/smallClassIcons/classicon_monk (1).jpg" },
-  { wowClass: "Paladin", value: "Paladin", icon: "src/assets/smallClassIcons/classicon_paladin.jpg" },
-  { wowClass: "Priest", value: "Priest", icon: "src/assets/smallClassIcons/classicon_priest (1).jpg" },
-  { wowClass: "Rogue", value: "Rogue", icon: "src/assets/smallClassIcons/classicon_rogue (1).jpg" },
-  { wowClass: "Shaman", value: "Shaman", icon: "src/assets/smallClassIcons/classicon_shaman (1).jpg" },
-  { wowClass: "Warlock", value: "Warlock", icon: "src/assets/smallClassIcons/classIcon_warlock.jpg" },
-  { wowClass: "Warrior", value: "Warrior", icon: "src/assets/smallClassIcons/classicon_warrior (1).jpg" },
+  { wowClass: "DK", value: "DK", icon: CLASSES.dkIcon },
+  { wowClass: "DH", value: "DH", icon: CLASSES.dhIcon },
+  { wowClass: "Druid", value: "Druid", icon: CLASSES.druidIcon },
+  { wowClass: "Evoker", value: "Evoker", icon: CLASSES.evokerIcon },
+  { wowClass: "Hunter", value: "Hunter", icon: CLASSES.hunterIcon },
+  { wowClass: "Mage", value: "Mage", icon: CLASSES.mageIcon },
+  { wowClass: "Monk", value: "Monk", icon: CLASSES.monkIcon },
+  { wowClass: "Paladin", value: "Paladin", icon: CLASSES.paladinIcon },
+  { wowClass: "Priest", value: "Priest", icon: CLASSES.priestIcon },
+  { wowClass: "Rogue", value: "Rogue", icon: CLASSES.rogueIcon },
+  { wowClass: "Shaman", value: "Shaman", icon: CLASSES.shamanIcon },
+  { wowClass: "Warlock", value: "Warlock", icon: CLASSES.warlockIcon },
+  { wowClass: "Warrior", value: "Warrior", icon: CLASSES.warriorIcon },
 ];
 
 export const ROLE_NAMES = [
