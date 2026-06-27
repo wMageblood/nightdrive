dcec96ea7c5f4b8dd9ce86eda33760177c11dc50
wMageblood <enzo.luciano@outlook.es>
2026-06-27 16:02:42 -0600

    testing 1024 and 1000

--- server/models/applications.ts
+++ server/models/applications.ts
@@ -21,7 +21,7 @@ export function getDataBase() {
     role: { type: String, trim: false, required: true },
     availability: { type: String, trim: true, maxlength: 150, required: true },
     contact: { type: String, trim: true, maxlength: 100, required: true },
-    experience: { type: String, trim: true, maxlength: 1000, required: true },
+    experience: { type: String, trim: true, maxlength: 1024, required: true },
     reasons: { type: String, trim: true, maxlength: 800, required: true },
     personal: { type: String, trim: true, maxlength: 800, required: true },
   });
